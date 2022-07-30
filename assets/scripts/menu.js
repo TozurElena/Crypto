a4c79910fd75d1ff5c5d3b9b167020ac3ec89562
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-07-30 11:37:08 +0200

    adaptiv section about

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -3,4 +3,10 @@ const headreNavigation = document.querySelector('.header__navigation');
 
 headerNavBtn.addEventListener('click', () => {
   headreNavigation.classList.toggle('header__navigation_open');
+});
+
+headreNavigation.addEventListener('click', ({target}) => {
+  if (target.classList.contains('navigation__link')) {
+    headreNavigation.classList.remove('header__navigation_open');
+  }
 })
