7a92232e1f7565432bdb7d9fc71fed0c14bdf551
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-07-31 12:23:50 +0200

    script pour section faq

---
+++ assets/scripts/faq.js
@@ -0,0 +1,42 @@
+const hide = (elem, answer) => {
+  if (!elem.classList.contains('.faq__item_show')) return;
+  answer.style.height = `${answer.offsetHeight}px`;
+  answer.offsetHeight;
+ answer.style.display = 'block';
+ answer.style.height = 0;
+ answer.style.overflow = 'hidden';
+ answer.style.transition = 'height 1.36s ease-in-out';
+  elem.classList.remove('.faq__item_show');
+ 
+}
+const show = (elem, answer) => {
+ if (elem.classList.contains('.faq__item_show')) return;
+
+ answer.style.display = 'block';
+ const height = answer.offsetHeight;
+ answer.style.height = 0;
+ answer.style.overflow = 'hidden';
+ answer.style.transition = 'height 1.36s ease-in-out';
+ answer.offsetHeight;
+ answer.style.height = `${height}px`;
+
+ elem.classList.add('.faq__item_show');
+}
+
+export const accordion = () => {
+  const list = document.querySelector('.faq__list');
+
+  list.addEventListener('click', e => {
+    // отлавливаем клик на кнопке с вопросом
+    const button = e.target.closest('.faq__question');
+
+    if (button) {
+      const item = button.closest('.faq__item');
+
+      // item.classList.toggle('faq__item_show');
+      const answer = item.querySelector('.faq__answer');
+      item.classList.contains('faq__item_show') ? hide(item, answer) : show(item, answer);
+    }
+
+  })
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,4 +2,8 @@ import './menu.js';
 import './modal.js';
 import './tabs_about-us.js';
 import './exchange-rates.js';
+import { accordion } from './faq.js';
+
+
+accordion();
 
